0e897ffb4e7b1645e08e42cd4f83b738a2c0c408
Anik Das <anikdas012@gmail.com>
2020-12-20 02:54:12 +0600

    Create new fragment for uploading details

---
+++ app/src/main/java/tk/anikdas/anikdas012/fieldbuzztest/ui/DetailsFragment.kt
@@ -0,0 +1,23 @@
+package tk.anikdas.anikdas012.fieldbuzztest.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+/**
+ * Created by "Anik Das" on 20-Dec-2020
+ * Developer email: "anikdas012@gmail.com"
+ */
+
+class DetailsFragment: Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+}
