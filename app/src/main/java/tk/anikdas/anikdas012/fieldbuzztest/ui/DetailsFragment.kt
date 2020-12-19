89b89acf123dca0ed4f8b2d9d32b0e89dd671bb8
Anik Das <anikdas012@gmail.com>
2020-12-20 05:10:31 +0600

    Validate inputs

--- app/src/main/java/tk/anikdas/anikdas012/fieldbuzztest/ui/DetailsFragment.kt
+++ app/src/main/java/tk/anikdas/anikdas012/fieldbuzztest/ui/DetailsFragment.kt
@@ -1,9 +1,11 @@
 package tk.anikdas.anikdas012.fieldbuzztest.ui
 
 import android.os.Bundle
+import android.util.Patterns
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import tk.anikdas.anikdas012.fieldbuzztest.databinding.FragmentDetailsBinding
@@ -41,6 +43,58 @@ class DetailsFragment: Fragment() {
     }
 
     private fun validateInputs() {
-        
+        if (binding.name.editText!!.text.isNullOrEmpty()) {
+            Toast.makeText(this.context, "Name is required", Toast.LENGTH_LONG).show()
+            return
+        }
+
+        if (binding.email.editText!!.text.isNullOrEmpty()
+                && Patterns.EMAIL_ADDRESS.matcher(binding.email.editText!!.text).matches()) {
+            Toast.makeText(this.context, "Proper email is required", Toast.LENGTH_LONG).show()
+            return
+        }
+
+        if (binding.phone.editText!!.text.isNullOrEmpty()) {
+            Toast.makeText(this.context, "Phone number is required", Toast.LENGTH_LONG).show()
+            return
+        }
+
+        if (binding.varsity.editText!!.text.isNullOrEmpty()) {
+            Toast.makeText(this.context, "University is required", Toast.LENGTH_LONG).show()
+            return
+        }
+
+        if (binding.year.editText!!.text.isNullOrEmpty()
+            && binding.year.editText!!.text.toString().toInt() !in 2015..2020
+        ) {
+            Toast.makeText(this.context, "Graduation year should be between 2015 and 2020", Toast.LENGTH_LONG).show()
+            return
+        }
+
+        if (binding.cgpa.editText!!.text.toString().toDouble() !in 2.0..4.0) {
+            Toast.makeText(this.context, "CGPA should be between 2.0 and 4.0", Toast.LENGTH_LONG).show()
+            return
+        }
+
+        if (binding.experience.editText!!.text.toString().toInt() !in 0..100) {
+            Toast.makeText(this.context, "Experience should be between 0 and 100 months", Toast.LENGTH_LONG).show()
+            return
+        }
+
+        if (binding.radioGroup.checkedRadioButtonId == -1) {
+            Toast.makeText(this.context, "Position is required", Toast.LENGTH_LONG).show()
+            return
+        }
+
+        if (binding.salary.editText!!.text.isNullOrEmpty()
+            && (binding.salary.editText!!.text.toString().toInt() !in 15000..60000)) {
+            Toast.makeText(this.context, "Salary should be between 150000 and 60000", Toast.LENGTH_LONG).show()
+            return
+        }
+
+        if (binding.url.editText!!.text.isNullOrEmpty()) {
+            Toast.makeText(this.context, "Github URL is required", Toast.LENGTH_LONG).show()
+            return
+        }
     }
 }
