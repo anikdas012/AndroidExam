166edc27d777affa22194124fe11b087a2570696
Anik Das <anikdas012@gmail.com>
2020-12-20 02:24:44 +0600

    Authenticate user and return token as a livedata object

--- app/src/main/java/tk/anikdas/anikdas012/fieldbuzztest/viewmodel/LoginViewModel.kt
+++ app/src/main/java/tk/anikdas/anikdas012/fieldbuzztest/viewmodel/LoginViewModel.kt
@@ -1,7 +1,12 @@
 package tk.anikdas.anikdas012.fieldbuzztest.viewmodel
 
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.LiveDataReactiveStreams
 import androidx.lifecycle.ViewModel
+import io.reactivex.schedulers.Schedulers
+import tk.anikdas.anikdas012.fieldbuzztest.models.User
+import tk.anikdas.anikdas012.fieldbuzztest.models.UserAuthenticate
+import tk.anikdas.anikdas012.fieldbuzztest.network.RetrofitService
 
 /**
  * Created by "Anik Das" on 20-Dec-2020
@@ -11,6 +16,16 @@ import androidx.lifecycle.ViewModel
 class LoginViewModel: ViewModel() {
 
     fun attemptLogin(username: String, password: String) : LiveData<String> {
-
+        val userAuthenticate = UserAuthenticate(username, password)
+        return LiveDataReactiveStreams.fromPublisher(
+            RetrofitService.getLoginApi().login(userAuthenticate)
+                .onErrorReturn { error ->
+                    return@onErrorReturn User("Error", "Error")
+                }
+                .map { user ->
+                    return@map user.token
+                }
+                .subscribeOn(Schedulers.io())
+        )
     }
 }
