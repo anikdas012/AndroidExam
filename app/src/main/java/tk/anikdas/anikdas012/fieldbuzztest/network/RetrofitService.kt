f0ccb5452ffbe9fd45b11e62498346bcfcae82c2
Anik Das <anikdas012@gmail.com>
2020-12-20 02:22:30 +0600

    Create Retrofit instance in a singleton manner

--- app/src/main/java/tk/anikdas/anikdas012/fieldbuzztest/network/RetrofitService.kt
+++ app/src/main/java/tk/anikdas/anikdas012/fieldbuzztest/network/RetrofitService.kt
@@ -1,5 +1,10 @@
 package tk.anikdas.anikdas012.fieldbuzztest.network
 
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
 
 /**
  * Created by "Anik Das" on 20-Dec-2020
@@ -7,4 +12,26 @@ package tk.anikdas.anikdas012.fieldbuzztest.network
  */
 
 class RetrofitService {
+
+    companion object {
+        private var retrofit: Retrofit? = null
+
+        private fun getRetrofitInstance() : Retrofit {
+            if (retrofit == null) {
+                val logging = HttpLoggingInterceptor()
+                logging.level = HttpLoggingInterceptor.Level.BODY
+
+                val okHttpClient = OkHttpClient.Builder()
+                    .addInterceptor(logging)
+
+                retrofit = Retrofit.Builder()
+                    .baseUrl("https://recruitment.fisdev.com/api/")
+                    .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .client(okHttpClient.build())
+                    .build()
+            }
+            return retrofit!!
+        }
+    }
 }
