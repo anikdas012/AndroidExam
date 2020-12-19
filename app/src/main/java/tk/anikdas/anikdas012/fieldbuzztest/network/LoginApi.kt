7f2d1069fa85519ea9002875e341c4f571256ad1
Anik Das <anikdas012@gmail.com>
2020-12-20 02:20:39 +0600

    Add new api endpoint for log in

--- app/src/main/java/tk/anikdas/anikdas012/fieldbuzztest/network/LoginApi.kt
+++ app/src/main/java/tk/anikdas/anikdas012/fieldbuzztest/network/LoginApi.kt
@@ -1,9 +1,18 @@
 package tk.anikdas.anikdas012.fieldbuzztest.network
 
+import io.reactivex.Flowable
+import retrofit2.http.Body
+import retrofit2.http.POST
+import tk.anikdas.anikdas012.fieldbuzztest.models.User
+import tk.anikdas.anikdas012.fieldbuzztest.models.UserAuthenticate
+
 /**
  * Created by "Anik Das" on 20-Dec-2020
  * Developer email: "anikdas012@gmail.com"
  */
 
 interface LoginApi {
+
+    @POST("login/")
+    fun login(@Body userAuthenticate: UserAuthenticate): Flowable<User>
 }
