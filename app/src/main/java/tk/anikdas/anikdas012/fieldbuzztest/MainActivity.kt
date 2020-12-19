113e4df1d7b5ea4ef0d2642f746a895fe96bff9e
Anik Das <anikdas012@gmail.com>
2020-12-19 23:04:38 +0600

    Navigate to LoginFragment

--- app/src/main/java/tk/anikdas/anikdas012/fieldbuzztest/MainActivity.kt
+++ app/src/main/java/tk/anikdas/anikdas012/fieldbuzztest/MainActivity.kt
@@ -2,10 +2,15 @@ package tk.anikdas.anikdas012.fieldbuzztest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import tk.anikdas.anikdas012.fieldbuzztest.ui.LoginFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportFragmentManager.beginTransaction()
+            .add(R.id.place_holder, LoginFragment(), "Login_Fragment")
+            .commit()
     }
 }
