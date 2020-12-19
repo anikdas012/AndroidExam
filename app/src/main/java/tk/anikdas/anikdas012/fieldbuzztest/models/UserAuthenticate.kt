1bd4fe748942a4c79d445920592d7639d283fbd3
Anik Das <anikdas012@gmail.com>
2020-12-20 02:17:40 +0600

    Create new class to send authentication data

---
+++ app/src/main/java/tk/anikdas/anikdas012/fieldbuzztest/models/UserAuthenticate.kt
@@ -0,0 +1,8 @@
+package tk.anikdas.anikdas012.fieldbuzztest.models
+
+/**
+ * Created by "Anik Das" on 20-Dec-2020
+ * Developer email: "anikdas012@gmail.com"
+ */
+
+data class UserAuthenticate(val username: String, val password: String)
