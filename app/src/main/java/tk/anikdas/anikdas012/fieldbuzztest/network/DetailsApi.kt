9c521f22366f4d81ece674031f8789a9fc84f711
Anik Das <anikdas012@gmail.com>
2020-12-20 06:31:11 +0600

    Add api endpoint for user details

--- app/src/main/java/tk/anikdas/anikdas012/fieldbuzztest/network/DetailsApi.kt
+++ app/src/main/java/tk/anikdas/anikdas012/fieldbuzztest/network/DetailsApi.kt
@@ -1,9 +1,20 @@
 package tk.anikdas.anikdas012.fieldbuzztest.network
 
+import io.reactivex.Flowable
+import retrofit2.http.Body
+import retrofit2.http.POST
+import tk.anikdas.anikdas012.fieldbuzztest.models.User
+import tk.anikdas.anikdas012.fieldbuzztest.models.UserAuthenticate
+import tk.anikdas.anikdas012.fieldbuzztest.models.UserDetails
+import tk.anikdas.anikdas012.fieldbuzztest.models.UserDetailsResponse
+
 /**
  * Created by "Anik Das" on 20-Dec-2020
  * Developer email: "anikdas012@gmail.com"
  */
 
 interface DetailsApi {
+
+    @POST("v0/recruiting-entities/")
+    fun login(@Body userDetails: UserDetails): Flowable<UserDetailsResponse>
 }
