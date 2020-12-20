bd2e766735bdfd7c84f6d49f6784d4f632168253
Anik Das <anikdas012@gmail.com>
2020-12-20 06:21:54 +0600

    Create new model for details api body

---
+++ app/src/main/java/tk/anikdas/anikdas012/fieldbuzztest/models/CV.kt
@@ -0,0 +1,10 @@
+package tk.anikdas.anikdas012.fieldbuzztest.models
+
+/**
+ * Created by "Anik Das" on 20-Dec-2020
+ * Developer email: "anikdas012@gmail.com"
+ */
+
+data class CV(
+    val tsync_id: String
+)
---
+++ app/src/main/java/tk/anikdas/anikdas012/fieldbuzztest/models/UserDetails.kt
@@ -0,0 +1,26 @@
+package tk.anikdas.anikdas012.fieldbuzztest.models
+
+/**
+ * Created by "Anik Das" on 20-Dec-2020
+ * Developer email: "anikdas012@gmail.com"
+ */
+
+data class UserDetails(
+    val tsync_id: String,
+    val name: String,
+    val email: String,
+    val phone: String,
+    val full_address: String?,
+    val name_of_university: String,
+    val graduation_year: Int,
+    val cgpa: Double?,
+    val experience_in_months: Int?,
+    val current_work_place_name: String?,
+    val applying_in: String,
+    val expected_salary: Int,
+    val field_buzz_reference: String?,
+    val github_project_url: String,
+    val cv_file: CV,
+    val on_spot_update_time: Int,
+    val on_spot_creation_time: Int
+)
