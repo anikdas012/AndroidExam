cba6d345c25834a111bf8345116df013ef502ff0
Anik Das <anikdas012@gmail.com>
2020-12-20 08:33:11 +0600

    Add methods to upload details and cv

--- app/src/main/java/tk/anikdas/anikdas012/fieldbuzztest/viewmodel/DetailsViewModel.kt
+++ app/src/main/java/tk/anikdas/anikdas012/fieldbuzztest/viewmodel/DetailsViewModel.kt
@@ -1,7 +1,20 @@
 package tk.anikdas.anikdas012.fieldbuzztest.viewmodel
 
+import android.content.Context
 import android.net.Uri
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.ViewModel
+import io.reactivex.schedulers.Schedulers
+import okhttp3.MediaType
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
+import tk.anikdas.anikdas012.fieldbuzztest.models.CVFile
+import tk.anikdas.anikdas012.fieldbuzztest.models.CVResponse
+import tk.anikdas.anikdas012.fieldbuzztest.models.UserDetails
+import tk.anikdas.anikdas012.fieldbuzztest.models.UserDetailsResponse
+import tk.anikdas.anikdas012.fieldbuzztest.network.RetrofitService
+import java.io.File
 
 /**
  * Created by "Anik Das" on 20-Dec-2020
@@ -12,6 +25,7 @@ class DetailsViewModel: ViewModel() {
 
     private lateinit var token: String
     private lateinit var cvUri: Uri
+    private lateinit var applicationContext: Context
 
     fun setToken(token: String) {
         this.token = token
@@ -19,5 +33,38 @@ class DetailsViewModel: ViewModel() {
 
     fun setCvUri(cvUri: Uri) {
         this.cvUri = cvUri
+    }
+
+    fun setApplicationContext(context: Context) {
+        applicationContext = context
+    }
+
+    fun uploadDetails(userDetails: UserDetails): LiveData<String> {
+        var status: MediatorLiveData<String> = MediatorLiveData()
+        RetrofitService.getDetailsApi().uploadDetails("Token $token", userDetails)
+            .onErrorReturn {
+                status.value = "Upload Error"
+                return@onErrorReturn UserDetailsResponse(CVFile(0, "Error"))
+            }
+            .map { it ->
+                val cvFile = it.cv_file
+                status.value = uploadCV(cvFile)
+            }
+            .subscribeOn(Schedulers.io())
+        return status
+    }
+
+    private fun uploadCV(cvFile: CVFile): String {
+        val originalFile = File(cvUri.path)
+        val filePart: RequestBody = RequestBody.create(MediaType.parse(applicationContext.contentResolver.getType(cvUri)!!), originalFile)
+        val file: MultipartBody.Part = MultipartBody.Part.createFormData("cv", originalFile.name, filePart)
+        val response = RetrofitService.getCVApi().uploadCV("Token $token",cvFile.id, file)
+            .onErrorReturn {
+                return@onErrorReturn CVResponse(0, false, "Error upload")
+            }
+            .subscribeOn(Schedulers.io())
+            .blockingFirst()
+
+        return response.message
     }
 }
